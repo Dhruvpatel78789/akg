ab3df37454414682c5d84c364ddabe31f31df2f0
Dhruvpatel78789 <76432810+Dhruvpatel78789@users.noreply.github.com>
2026-07-31 03:52:56 +0530

    clean up

--- scratch/check-keys.js
+++
@@ -1,5 +0,0 @@
-const { loadEnvConfig } = require("@next/env");
-loadEnvConfig(process.cwd());
-
-console.log("RAZORPAY_KEY_ID prefix:", process.env.RAZORPAY_KEY_ID ? process.env.RAZORPAY_KEY_ID.substring(0, 8) : "NOT_FOUND");
-process.exit(0);
